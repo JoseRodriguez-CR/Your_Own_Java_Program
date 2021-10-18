173321c984f2fc864464856f3f71bfb9ee4e0f9a
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-10-18 11:47:52 -0600

    Uploading Files

---
+++ PersonalInformation.java
@@ -0,0 +1,8 @@
+
+public class PersonalInformation {
+    public static void main(String[] args) {
+        System.out.println("My name is Jose Mario Rodriguez");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometown is San Jose, CR");
+    }
+}
